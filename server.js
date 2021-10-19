1a869fa57beb11fc1d0e97478fba560067eb299d
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-19 17:33:34 +0100

    set up server connections and various middleware
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ const home = require('./routes/home.js');
 
 const server = express();
 
+const cookieParser = require('cookie-parser');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+server.use(cookieParser(process.env.COOKIE_SECRET));
+
+server.use(express.urlencoded({ extended: false }));
+
 const staticHandler = express.static('public');
 server.use(staticHandler);
 
 server.get('/', home.get);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
