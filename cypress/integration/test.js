8316f5f795369449347b2ee527d5580ba6394acb
danilo-cupido <danilocupido@gmail.com>
2021-10-19 11:31:40 +0100

    create basic testing
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

---
+++ cypress/integration/test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// beforeEach(() => {
+//   cy.task('resetDb');
+// });
+
+// describe('home page', () => {
+//   it('displays Sign up and Log in', () => {
+//     cy.visit('/');
+//     cy.contains('Sign up');
+//     cy.contains('Log in');
+//   });
+// });
+
+describe('home page', () => {
+  it('displays Sign up and Log in', () => {
+    cy.visit('www.google.com');
+  });
+});
