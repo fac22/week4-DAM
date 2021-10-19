03f150f588aeb872a89973455f8d259f35f5e7e5
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-19 11:21:28 +0100

    install nodemon and added dev script

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,8 @@
-const layout = require("../layout");
-
 function get(request, response) {
-	const html = /*html*/ `
+  const html = /*html*/ `
       <h1>Hello</h1>
     `;
-	response.send(layout("Home", html));
+  response.send(html);
 }
 
 module.exports = { get };
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const home = require('./routes/home.js');
+
+const server = express();
+
+const staticHandler = express.static('public');
+server.use(staticHandler);
+
+server.get('/', home.get);
+
+const PORT = 3000;
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
