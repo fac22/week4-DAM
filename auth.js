a23681452b8586b9049da55fa571f77c4d5f1574
danilo-cupido <danilocupido@gmail.com>
2021-10-20 11:34:06 +0100

    rearrenge the order of the functions
    Co-authored-by: Mohamed Ali Siyad <Alisyad9@users.noreply.github.com>

--- auth.js
+++ auth.js
@@ -4,13 +4,6 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const model = require('./database/model.js');
 
-const COOKIE_OPTIONS = {
-  httpOnly: true,
-  maxAge: 600000,
-  sameSite: 'strict',
-  signed: true,
-};
-
 function createUser(username, email, password) {
   return bcrypt
     .hash(password, 10)
@@ -21,5 +14,12 @@ function saveUserSession(user) {
   const sid = crypto.randomBytes(18).toString('base64');
   return model.createSession(sid, { user });
 }
+
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  maxAge: 600000,
+  sameSite: 'strict',
+  signed: true,
+};
 
 module.exports = { createUser, saveUserSession, COOKIE_OPTIONS };
