1e758175fe82d8925aba3ffa00eb6f8c82b7c64d
danilo-cupido <danilocupido@gmail.com>
2021-10-20 11:36:53 +0100

    implement createUser and createSession
    Co-authored-by: Mohamed Ali Siyad <Alisyad9@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -2,4 +2,22 @@
 
 const db = require('./connection.js');
 
-module.exports = {};
+function createUser(username, email, hash) {
+  const INSERT_USER = `
+  INSERT INTO users (username, email, password) VALUES ($1, $2, $3)
+  RETURNING id, email, username
+  `;
+  return db
+    .query(INSERT_USER, [username, email, hash])
+    .then((result) => result.rows[0]);
+}
+
+function createSession(sid, json) {
+  const INSERT_SESSION = ` INSERT INTO sessions (sid, data) VALUES ($1, $2)
+    RETURNING sid`;
+  return db
+    .query(INSERT_SESSION, [sid, json])
+    .then((response) => response.rows[0].sid);
+}
+
+module.exports = { createUser, createSession };
