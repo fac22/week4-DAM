7e6a0548704abd180bb4cc837a44c6c13da5d7fc
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-21 11:21:16 +0100

    created catPage db query and page builder

--- routes/catPage.js
+++ routes/catPage.js
@@ -3,7 +3,19 @@
 const model = require('../database/model.js');
 
 function get(req, res) {
-  res.send('hello');
+  const user = req.session;
+  model
+    .getCat(req.params.id)
+    .then((result) => result.rows[0])
+    .then((cat) =>
+      res.send(/* html */ `
+        <h2>${cat.name}</h2>
+        <img src="/cats/${cat.id}/avatar" alt="Picture of ${cat.name}" />
+        <p>Added by ${
+          cat.username === user.username ? 'you' : cat.username
+        } on ${cat.created_at}</p>
+      `)
+    );
 }
 
 module.exports = { get };
